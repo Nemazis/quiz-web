b3ce57bf993c810e715fd38f41313561211021f9
nemazis <tommygreathead711@hotmail.com>
2023-02-21 14:52:51 +0000

    changed back-end

--- src/services/config.service.js
+++ src/services/config.service.js
@@ -2,7 +2,7 @@ angular.module('app').factory('ConfigService', [function() {
 
     var service = {};
 
-    service.apiBase = 'http://localhost:8001/';
+    service.apiBase = 'https://quizapifoob-env.eba-hbggmih7.eu-west-2.elasticbeanstalk.com/';
 
     return service;
 }]);
